6cc73dc57c91e8d42c89a8842ed70d6e256541cd
azielazny <azielazny@gmail.com>
2017-05-09 21:34:27 +0200

    add form for register, coments for tab, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import {ModuleWithProviders}             from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {MainPageComponent}      from './main-page/main-page.component';
-import {EventDetailsComponent}  from './event-detail/event-detail.component';
+import {EventsDetailsComponent}  from './events-details/events-details.component';
 // import {EventsComponent}  from './event-detail/events.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/index', pathMatch: 'full' },
   {path: 'index', component: MainPageComponent},
-  {path: 'event-detail/:id', component: EventDetailsComponent},
+  {path: 'event-detail/:id', component: EventsDetailsComponent},
   // {path: 'Events', component: Events}
 ];
 
