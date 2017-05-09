7c7532b5955af82db34bdfdd611a0deff7e37102
azielazny <azielazny@gmail.com>
2017-05-09 20:20:05 +0200

    6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import {ModuleWithProviders}             from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {MainPageComponent}      from './main-page/main-page.component';
-import {EventDetailComponent}  from './event-detail/event-detail.component';
+import {EventDetailsComponent}  from './event-detail/event-detail.component';
 // import {EventsComponent}  from './event-detail/events.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/index', pathMatch: 'full' },
   {path: 'index', component: MainPageComponent},
-  {path: 'event-detail/:id', component: EventDetailComponent},
+  {path: 'event-detail/:id', component: EventDetailsComponent},
   // {path: 'Events', component: Events}
 ];
 
