51e82b478e9081679028296063f2091287bcbf13
Szymon Oracki <szymon.oracki@oustish.pl>
2017-05-11 20:07:31 +0200

    WIP3

--- src/app/events-right-column/event-right-column.component.ts
+++ src/app/events-right-column/event-right-column.component.ts
@@ -12,10 +12,36 @@ export class EventsRightColumnComponent implements OnInit {
   private eventdata: Event;
   private events: Event[] = [];
 
+  location: string = "";
+
   constructor(public route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.events.push(this.eventdata);
+
+
+
+    this.location = "";
+
+    if (this.eventdata.loc_name != null)
+      this.location += this.eventdata.loc_name.trim() + ', ';
+
+    if (this.eventdata.loc_street != null)
+      this.location += this.eventdata.loc_street.trim();
+
+    if (this.eventdata.loc_street != null && this.eventdata.loc_bnum != null)
+      this.location += " ";
+
+    if (this.eventdata.loc_bnum != null)
+      this.location += this.eventdata.loc_bnum.trim();
+
+    if ((this.eventdata.loc_street != null || this.eventdata.loc_bnum != null) && this.eventdata.loc_city != null)
+      this.location += ', ';
+
+    if (this.eventdata.loc_city != null)
+      this.location += this.eventdata.loc_city.trim();
+
+    this.location = this.location.toUpperCase();
   }
 
 }
