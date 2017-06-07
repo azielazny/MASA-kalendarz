86182a9f9d86baa743ad48545b28fd1b24253f24
azielazny <azielazny@gmail.com>
2017-06-07 13:22:56 +0200

    weathermap widget -working version

--- src/app/shared/weather-widget/weather-widget.component.ts
+++ src/app/shared/weather-widget/weather-widget.component.ts
@@ -1,36 +1,45 @@
-import {Component, Input, OnInit, OnChanges, AfterViewInit, Inject} from '@angular/core';
-
-declare var $:JQueryStatic;
+import {Component, Input, OnInit, OnChanges, AfterViewInit, Inject, NgZone, HostBinding} from '@angular/core';
+import {Event} from '../../class/event.class';
+declare var $: JQueryStatic;
 
 @Component({
   selector: 'weather-widget',
   templateUrl: 'weather-widget.component.html',
-  styleUrls: ['weather-widget.component.css']
+  styleUrls: ['weather-widget.component.scss']
 })
 
 
 export class WeatherWidgetComponent implements OnInit, AfterViewInit {
-   myWidgetParam: {id: number; event_date: number; city_name: string; appid: string; units: string; containerid: string};
-   x:any;
+  @Input()
+  private _eventdata: Event;
 
-  constructor() {
+  private now: any = new Date();
+  private maxDate: number = (this.now.getTime() + 15 * 24 * 60 * 60 * 1000);
+  private _weatherappid = '9099fdbbe0e4de40bcfe29b960187173';
+  private _weatherunits = 'metric';
+  private _weathercontainer = 'openweathermap-widget';
+  private _weatherApiFile = "app/shared/weather-widget/WeatherFromApi.js";
 
+  @HostBinding('class.hidden') hiddenStyle: boolean = false;
+
+  constructor(private zone: NgZone) {
   }
-  ngAfterViewInit() {
-    // $( document ).ready(function() {
 
-    this.myWidgetParam = {
-      id: 21,
-      event_date: 1496898730,
-      city_name: "Gdańsk",
-      appid: '9099fdbbe0e4de40bcfe29b960187173',
-      units: 'metric',
-      containerid: 'openweathermap-widget'
-    };
-      this.x=require("app/shared/weather-widget/WeatherFromApi.js");
-    // });
-  }
   ngOnInit() {
+    if (this._eventdata.start_ts >= this.now.getTime() && this._eventdata.start_ts <= this.maxDate)
+      this.hiddenStyle = true;
+  }
+
+  ngAfterViewInit() {
+    (<any>window).myWidgetParam = {
+      id: 21,
+      event_date: this._eventdata.start_ts,
+      city_name: this._eventdata.loc_city.trim(),
+      appid: this._weatherappid,
+      units: this._weatherunits,
+      containerid: this._weathercontainer
+    };
+    require(this._weatherApiFile);
 
   }
 
