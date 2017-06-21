5c5ddb1e5b35875555867e66b6802984ce4fd856
azielazny <azielazny@gmail.com>
2017-06-21 21:52:26 +0200

    merge2

--- src/app/calendar/calendar-edit-events-public-settings/calendar-edit-events-public-settings.component.ts
+++ src/app/calendar/calendar-edit-events-public-settings/calendar-edit-events-public-settings.component.ts
@@ -8,23 +8,37 @@ import {User} from "../../class/user.class";
   styleUrls: ['calendar-edit-events-public-settings.component.scss']
 })
 export class CalendarEditEventsPublicSettingsComponent  {
+  private eventImage:string;
+
 
   constructor() {
   }
-
   @ViewChild('uploadBtn') uploadBtn;
-
   @Output() pictureChange = new EventEmitter();
 
-  fileChanged(e) {
-    if(this.uploadBtn.nativeElement.files.length != 1) return;
-    let x = this;
+  // fileChanged(e) {
+  //   if(this.uploadBtn.nativeElement.files.length != 1) return;
+  //   let x = this;
+  //
+  //   let reader = new FileReader();
+  //   reader.onloadend = function() {
+  //     this.picture=
+  //     x.pictureChange.emit(reader.result);
+  //   };
+  //   reader.readAsDataURL(this.uploadBtn.nativeElement.files[0]);
+  // }
 
-    let reader = new FileReader();
-    reader.onloadend = function() {
-      x.pictureChange.emit(reader.result);
-    };
-    reader.readAsDataURL(this.uploadBtn.nativeElement.files[0]);
+  fileChanged($event) : void {
+    this.readThis($event.target);
   }
+  readThis(inputValue: any): void {
+    let file:File = inputValue.files[0];
+    let reader:FileReader = new FileReader();
 
+    reader.onloadend = (e) => {
+      this.eventImage= reader.result;
+      this.pictureChange.emit(reader.result)
+    }
+    reader.readAsDataURL(file);
+  }
 }
