80e06b6535e4dfd83233a31a1fe12a6f3fee1f97
azielazny <azielazny@gmail.com>
2017-06-23 20:35:21 +0200

    pierwszy merge

--- src/app/calendar/calendar-right-column/calendar-right-column.component.ts
+++ src/app/calendar/calendar-right-column/calendar-right-column.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input, NgZone, OnChanges, Output, EventEmitter} from '@angular/core';
 import {EventsService} from "../../services/events.service";
 import {Event} from "../../class/event.class";
+import {Category} from "../../class/category.class";
 
 @Component({
   selector: 'calendar-right-column',
@@ -15,6 +16,7 @@ export class CalendarRightColumnComponent implements OnInit, OnChanges {
   private limitEvents: number;
   private eventsToShow: Event[] = [];
   public shown = false;
+  private categoriesList: Category[] = [];
 
   @Input()
   public selectedDate;
@@ -24,7 +26,7 @@ export class CalendarRightColumnComponent implements OnInit, OnChanges {
 
   @Output() outputEventId: EventEmitter<number> = new EventEmitter();
 
-  constructor(private eventsService: EventsService) {
+  constructor(private eventsService: EventsService, private categoriesService) {
     this.height = <any>window.innerHeight;
     this.limitEvents = this.limitEventsToShow();
   }
